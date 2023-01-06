b7053a46340e0b4d786151d716f589cd60ac4d7d
CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2023-01-06 17:24:52 -0500

    Create newsFeed/profilePage

--- AntraShare/src/app/features/features.module.ts
+++ AntraShare/src/app/features/features.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NewsFeedComponent,
+    ProfilePageComponent
+  ],
   imports: [
     CommonModule
   ]

ea8d68c7a2a33104bbda544956f135bb448e42cb
Tianshu Xiao <tx2@g.ucla.edu>
2023-01-07 11:54:06 -0800

    init setting page

--- AntraShare/src/app/features/features.module.ts
+++ AntraShare/src/app/features/features.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingPageComponent } from './setting-page/setting-page.component';
-import { AdminPageComponent } from './admin-page/admin-page.component';
 
 
 
 @NgModule({
   declarations: [
     SettingPageComponent,
-    AdminPageComponent
   ],
   imports: [
     CommonModule

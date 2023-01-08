81e45e845357ca6512eafc223ece19981b175360
Liaoshousan <shousanl@andrew.cmu.edu>
2023-01-08 11:53:09 -0500

    build file structure; add nav-bar as shared components

--- AntraShare/src/app/features/features.module.ts
+++ AntraShare/src/app/features/features.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { SettingPageComponent } from './setting-page/setting-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent,
+    NewsFeedComponent,
+    ProfilePageComponent,
+    SettingPageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LoginPageComponent,
+    NewsFeedComponent,
+    ProfilePageComponent,
+    SettingPageComponent
   ]
 })
 export class FeaturesModule { }
---
+++ AntraShare/src/app/features/setting-page/setting-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setting-page',
+  templateUrl: './setting-page.component.html',
+  styleUrls: ['./setting-page.component.scss']
+})
+export class SettingPageComponent {
+
+}

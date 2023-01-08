166f71b86281a2debd6934b60926ab2d980d61ad
XiaomenggNiu <xniu5@ur.rochester.edu>
2023-01-08 14:56:05 -0500

    Admin and Setting page initialized

--- AntraShare/src/app/features/features.module.ts
+++ AntraShare/src/app/features/features.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SettingPageComponent } from './setting-page/setting-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SettingPageComponent,
+    AdminPageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SettingPageComponent,
+    AdminPageComponent
   ]
 })
 export class FeaturesModule { }

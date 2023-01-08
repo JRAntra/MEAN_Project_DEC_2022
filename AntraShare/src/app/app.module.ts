15a124383d9a1efcf21efb895237155f8e7efeda
Liaoshousan <shousanl@andrew.cmu.edu>
2023-01-08 16:30:48 -0500

    introduce nav bar to app component

--- AntraShare/src/app/app.module.ts
+++ AntraShare/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,

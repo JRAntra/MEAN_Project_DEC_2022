b24833e9e2b9ddf4240802b0d9a61a7804f539f3
Liaoshousan <shousanl@andrew.cmu.edu>
2023-01-08 16:36:43 -0500

    add routing to render each component

--- AntraShare/src/app/app-routing.module.ts
+++ AntraShare/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { NewsFeedComponent } from './features/news-feed/news-feed.component';
+import { ProfilePageComponent } from './features/profile-page/profile-page.component';
+import { SettingPageComponent } from './features/setting-page/setting-page.component';
+const routes: Routes = [
+  { path: 'profiles', component: ProfilePageComponent },
+  { path: 'news', component: NewsFeedComponent },
+  { path: 'settings', component: SettingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

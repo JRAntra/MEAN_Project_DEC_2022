ab1451355ae23f73d891b9e5377e961bb9202e58
Robin14ZLP <robinzhang9892@gmail.com>
2023-01-08 20:30:13 -0500

    include angular materail package

--- AntraShare/src/app/features/features.module.ts
+++ AntraShare/src/app/features/features.module.ts
@@ -4,18 +4,26 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { NewsFeedComponent } from './news-feed/news-feed.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { SettingPageComponent } from './setting-page/setting-page.component';
-
-
+import { LikeListBarComponent } from './news-feed/like-list-bar/like-list-bar.component';
+import { NewPostComponent } from './news-feed/new-post/new-post.component';
+import { StoryComponent } from './news-feed/story/story.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     LoginPageComponent,
     NewsFeedComponent,
     ProfilePageComponent,
-    SettingPageComponent
+    SettingPageComponent,
+    LikeListBarComponent,
+    NewPostComponent,
+    StoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatCardModule
   ],
   exports: [
     LoginPageComponent,

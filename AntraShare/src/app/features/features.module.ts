757a64af5ed4cfe47ca676d0bb23ed66960ac78d
Yiwen <yiwen@MacBook-Pro.local>
2023-01-10 12:59:36 -0500

    new changes

--- AntraShare/src/app/features/features.module.ts
+++ AntraShare/src/app/features/features.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { SettingPageComponent } from './setting-page/setting-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { NewsfeedPageComponent } from './newsfeed-page/newsfeed-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    SettingPageComponent,
+    AdminPageComponent,
+    NewsfeedPageComponent,
+    ProfilePageComponent
   ],
   imports: [
     CommonModule

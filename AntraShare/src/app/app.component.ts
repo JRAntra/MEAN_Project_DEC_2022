3af775ab373b8932844454cf7d9cb5d4c72aa581
Yusheng Zhang <yusheng.zhang@antra.com>
2023-01-10 14:46:38 -0500

    formcontrol, formGroup, formArray

--- AntraShare/src/app/app.component.ts
+++ AntraShare/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'AntraShare';
+  text = "this is a demo for data Bindings"
+  textContent = "lastname"
+  styleVar = ""
+
+  ngOnInit(): void {
+    // console.log(this.text)
+  }
+
+  onClickRed(): string{
+   
+    return "" 
+  }
+
+  onClickButton():void{
+    alert("button clicked")
+  }
+
+  onChangeInputText(): void {
+    alert("text changed")
+    FormControl
+  }
+    
 }

64b3f7f954f5f78b1cd93bc5ab9f551a5fa82828
Yusheng Zhang <yusheng.zhang@antra.com>
2023-01-10 14:46:46 -0500

    addup

--- AntraShare/src/app/app.module.ts
+++ AntraShare/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DataBindingComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LoginModule,
+    FormsModule,
+    ReactiveFormsModule
+
  
   ],
   providers: [],
---
+++ AntraShare/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+
+  // firstname: FormControl = new FormControl('JR')
+  // lastname: FormControl = new FormControl('Zhang')
+  
+  // nameField: FormGroup = new FormGroup({
+  //   firstname: new FormControl('JR'),
+  //   lastname: new FormControl('Zhang')
+  // })
+
+  nameField : FormGroup = new FormGroup({})
+  jobs: FormGroup = new FormGroup({})
+
+  constructor(private fb: FormBuilder) {
+
+   }
+
+  name: string = ''
+  ngOnInit(): void {
+
+    this.nameField = this.fb.group({
+      firstname: 'JR',
+      lastname:'Zhang',
+      contacts: this.fb.group({
+        phoneNum: '00000000000',
+        address:'Sterling'
+      }),
+      career: this.fb.array(
+      [
+        this.jobs = this.fb.group({
+               
+          title: 'web developer',
+          period: this.fb.group({
+            
+          })
+        
+        })
+  
+      ]
+      )
+    })
+
+    this.name = 'JR'
+    this.nameField.get('firstname')?.setValue('David')
+    console.log(this.nameField)
+
+  }
+
+  displayValue() {
+    console.log(this.nameField)
+    // this.firstname.valueChanges.subscribe
+  }
+  clearValue(){
+    this.name = 'cleared'
+  }
+  submitForms(){
+
+  }
+
+  get firstname(): FormControl{
+    return this.nameField.get('firstname') as FormControl
+  }
+
+
+}

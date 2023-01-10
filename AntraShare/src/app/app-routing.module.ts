44bf93e8d1731e9b59b6414d5458eb9decdaf445
XiaomenggNiu <xniu5@ur.rochester.edu>
2023-01-10 12:29:16 -0500

    Add routing and Angular material

--- AntraShare/src/app/app-routing.module.ts
+++ AntraShare/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SettingPageComponent } from './features/setting-page/setting-page.component';
+import { AdminPageComponent } from './features/admin-page/admin-page.component';
+import { ProfilePageComponent } from './features/profile-page/profile-page.component';
+import { NewsfeedPageComponent } from './features/newsfeed-page/newsfeed-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'settings', component: SettingPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'newsfeed', component: NewsfeedPageComponent },
+  { path: 'profile', component: ProfilePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AntraShare/src/app/app.module.ts
+++ AntraShare/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeaturesModule } from './features/features.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { FeaturesModule } from './features/features.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FeaturesModule
+    FeaturesModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

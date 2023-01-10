62b52c625e7a2d9cbcd711ca2c3fcac710122ec5
CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2023-01-10 16:16:16 -0500

    modification with JR on 1/10

--- AntraShare/src/app/app.module.ts
+++ AntraShare/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeaturesModule } from './features/features.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FeaturesModule } from './features/features.module';
     BrowserModule,
     AppRoutingModule,
     FeaturesModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

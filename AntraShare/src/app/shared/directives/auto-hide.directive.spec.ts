b847a3dcdc51451db66a375ac98b14b826cfa16d
XiaomenggNiu <xniu5@ur.rochester.edu>
2023-01-20 15:42:00 -0500

    Remove customized directive

--- AntraShare/src/app/shared/directives/auto-hide.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AutoHideDirective } from './auto-hide.directive';
-
-describe('AutoHideDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutoHideDirective();
-    expect(directive).toBeTruthy();
-  });
-});

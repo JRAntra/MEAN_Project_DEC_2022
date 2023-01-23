9d1b278a2c6b7c38712017fa6f009a728b37ce1b
Tianshu Xiao <tx2@g.ucla.edu>
2023-01-22 20:48:58 -0800

    login page validators init

--- AntraShare/src/app/features/login-page/login-page.component.ts
+++ AntraShare/src/app/features/login-page/login-page.component.ts
@@ -18,7 +18,7 @@ export class LoginPageComponent implements OnInit {
   ngOnInit() {
     this.loginForm = new FormGroup({
       'username': new FormControl(null, Validators.required),
-      'password': new FormControl(null, Validators.required),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
     });
   }
 

822b97bc0022038ac8b1529e14806313cbbb4b9c
XiaomenggNiu <xniu5@ur.rochester.edu>
2023-01-23 13:15:43 -0500

    Bugs created

--- AntraShare/src/app/features/profile-page/profile-page-component/profile-page.component.ts
+++ AntraShare/src/app/features/profile-page/profile-page-component/profile-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { map, Observable } from 'rxjs';
 import { UsernameService } from '../service/username.service';
 
@@ -11,19 +11,21 @@ import { UsernameService } from '../service/username.service';
 export class ProfilePageComponent implements OnInit{
   genders: string[] = ['Male', 'Female', 'Prefer not to say'];
   hide = true;
-  profileForm = new FormGroup({
-    username: new FormControl('', {validators: [Validators.required]}),
-    password: new FormControl('', {validators: [Validators.required, this.oneUppercase, 
-        this.oneSpecialChar]}),
-    passwordConfirm: new FormControl('', {validators: [Validators.required, this.passwordsMatch]}),
-    age: new FormControl(),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl(),
-    gender: new FormControl()
-  });
-  constructor(private usernameService: UsernameService) { }
+  profileForm = new FormGroup({});
+
+  constructor(private fb: FormBuilder, private usernameService: UsernameService) { }
+  
   ngOnInit(): void {
-    this.profileForm
+    this.profileForm = this.fb.group({
+        username: [' ', [Validators.required, this.checkUsername]],
+        password: [' ', [Validators.required, this.oneUppercase, 
+            this.oneSpecialChar]],
+        passwordConfirm: ['', [Validators.required, this.passwordsMatch]],
+        // age: [''],
+        email: [' ', [Validators.required, Validators.email]],
+        // phone: [''],
+        // gender: [''],
+      })
   }
 
   onSubmit(): void{
@@ -34,20 +36,25 @@ export class ProfilePageComponent implements OnInit{
     const email = this.profileForm.get('email')?.value;
     const phone = this.profileForm.get('phone')?.value;
 
-    console.log(password)
+    this.profileForm.setValue({
+      userName: username,
+      password: password,
+      gender: gender,
+      age: age,
+      userEmail: email,
+      phone: phone
+    });
   }
 
-  checkUsername(control: AbstractControl): Observable<ValidationErrors | null>{
-    console.log("In chekc function", this.usernameService);
-    return this.usernameService.checkExist(control.value).pipe(
-      map(res => {
-        console.log(res);
-        if(res) {
-          return { "usernameExists": "true"};
-        } else {
-          return null;
-        }
-    }))
+  checkUsername(){
+      return (control: AbstractControl) => {
+        this.usernameService.checkExist(control.value).subscribe(
+          ans => {
+            if (ans) {
+              control.setErrors({'usernameExists': true})
+            }
+          })
+      }
   }
 
   oneUppercase(control: AbstractControl): ValidationErrors | null  {
@@ -67,19 +74,14 @@ export class ProfilePageComponent implements OnInit{
     }
   }
 
-  passwordsMatch(control: AbstractControl): ValidationErrors | null {
-    var password = control.get('password')?.value;
-    var passwordConfirmation = control.get('passwordConfirm')?.value;
-    console.log('1', password)
-    console.log('2', passwordConfirmation)
-    if (password === passwordConfirmation) {
-      console.log("matched")
-      control.get('passwordConfirm')?.setErrors(null);
-      return null;
-    } else {
-      console.log("not matched")
-      control.get('passwordConfirm')?.setErrors({ passwordsMatch: false} );
-      return { "passwordsMatch": "false" };
+  passwordsMatch(match: FormControl){
+    return (control: AbstractControl) => {
+      const password = control.value;
+      const passwordConfirm = match.value;
+
+      if (password !== passwordConfirm){
+        match.setErrors({'passwordsMatch': false})
+      }
     }
   }
 
--- AntraShare/src/app/features/profile-page/service/username.service.ts
+++ AntraShare/src/app/features/profile-page/service/username.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { map, Observable, Subject } from 'rxjs';
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,17 @@ export class UsernameService {
       output.next(getResult);
     });
     return output.asObservable();
+  }
+  validateUsername(): AsyncValidatorFn{
+    console.log("Validator func")
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      console.log(control.value)
+      return this.checkExist(control.value).pipe(
+        map(res => {
+          return {'usernameExists': res};
+        })
+      )
+    }
+    
   }
 }

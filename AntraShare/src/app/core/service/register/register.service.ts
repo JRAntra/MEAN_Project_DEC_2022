4180d994060f5542146677c01550ec09b56d726b
Liaoshousan <shousanl@andrew.cmu.edu>
2023-01-23 14:36:27 -0500

    create validor for register service (lack pw match functionality)

--- AntraShare/src/app/core/service/register/register.service.ts
+++ AntraShare/src/app/core/service/register/register.service.ts
@@ -1,21 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterService {
-  private registerUrl = 'http://localhost:4231/api/register/createNewAccount';
+  private createNewAccountUrl =
+    'http://localhost:4231/api/register/createNewAccount';
+  private checkUserNameExistUrl =
+    'http://localhost:4231/api/register/checkExistByUsername/';
+
+  private checkEmailUsedUrl =
+    'http://localhost:4231/api/register/checkExistByEmail/';
 
   constructor(private http: HttpClient, private router: Router) {}
   register(user: user): Observable<any> {
-    return this.http.post(this.registerUrl, user);
+    return this.http.post(this.createNewAccountUrl, user);
+  }
+  checkUserNameExist(userName: string | null | undefined): Observable<any> {
+    return this.http.get(this.checkUserNameExistUrl + userName);
+  }
+
+  checkEmailUsed(email: string | null | undefined): Observable<any> {
+    return this.http.get(this.checkEmailUsedUrl + email);
   }
 }
 
 type user = {
-  userName: string;
-  userEmail: string;
-  password: string;
+  userName: string | null | undefined;
+  userEmail: string | null | undefined;
+  password: string | null | undefined;
 };
--- AntraShare/src/app/features/register-page/register-page.component.ts
+++ AntraShare/src/app/features/register-page/register-page.component.ts
@@ -1,47 +1,111 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription, first } from 'rxjs';
 import { RegisterService } from 'src/app/core/service/register/register.service';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.scss'],
 })
-export class RegisterPageComponent {
-  form = new FormGroup({
-    userName: new FormControl(),
-    password: new FormControl(),
-    passwordcon: new FormControl(),
-    email: new FormControl(),
-  });
+export class RegisterPageComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
   subscription: Subscription = new Subscription();
 
   constructor(
     private router: Router,
-    private registerService: RegisterService
+    private registerService: RegisterService,
+    private formBuider: FormBuilder
   ) {}
+  ngOnInit(): void {
+    this.form = this.formBuider.group({
+      userName: new FormControl('', {
+        validators: [Validators.required, this.userNameExist()],
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          this.hasUpperAndLower,
+          this.hasSpecial,
+        ],
+      }),
+      passwordcon: new FormControl('', [Validators.required]),
+      email: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.email,
+          this.userEmailExist(),
+        ],
+      }),
+    });
+  }
+
+  hasUpperAndLower(control: AbstractControl): ValidationErrors | null {
+    const hasUpper = /[A-Z]/.test(control.value);
+    const hasLower = /[a-z]/.test(control.value);
+    return hasUpper && hasLower ? null : { hasUpperAndLower: false };
+  }
+
+  hasSpecial(control: AbstractControl): ValidationErrors | null {
+    const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(control.value);
+    return hasSpecial ? null : { hasSpecial: hasSpecial };
+  }
+
+  // TODO: Can't get password value to compare with passwordcon
+  // isPasswordMatched(control: AbstractControl): ValidationErrors | null {
+  //   console.log(control.get('password')); // this will get null
+  //   // const matched = this.form.get('password')?.value === control.value;
+  //   const matched = true;
+  //   return matched ? null : { match: false };
+  // }
+
+  userEmailExist() {
+    return (control: AbstractControl) => {
+      if (control.value.length !== 0) {
+        this.registerService.checkEmailUsed(control.value).subscribe((val) => {
+          console.log(val);
+          if (val) control.setErrors({ exist: true });
+        });
+      }
+      return null;
+    };
+  }
+
+  userNameExist() {
+    return (control: AbstractControl) => {
+      if (control.value.length !== 0) {
+        this.registerService
+          .checkUserNameExist(control.value)
+          .subscribe((val) => {
+            if (val) control.setErrors({ exist: true });
+          });
+      }
+      return null;
+    };
+  }
+
   onSubmit() {
     const registerData: user = {
       userName: this.form.get('userName')?.value,
       userEmail: this.form.get('email')?.value,
       password: this.form.get('password')?.value,
     };
-    this.subscription = this.registerService.register(registerData).subscribe(
-      (response) => {
-        console.log(response);
+
+    this.registerService
+      .register(registerData)
+      .pipe(first())
+      .subscribe((val) => {
+        alert('Registration successful');
+        console.log(val);
         this.goToLoginPage();
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-    // this.form.setValue({
-    //   userName: '',
-    //   password: '',
-    //   passwordcon: '',
-    //   email: '',
-    // });
+      });
   }
 
   goToLoginPage() {
@@ -54,7 +118,7 @@ export class RegisterPageComponent {
 }
 
 type user = {
-  userName: string;
-  userEmail: string;
-  password: string;
+  userName: string | null | undefined;
+  userEmail: string | null | undefined;
+  password: string | null | undefined;
 };

f7ac1390dc799392d3c141dc7ce14601a596f73c
Tianshu Xiao <tx2@g.ucla.edu>
2023-01-29 21:52:05 -0800

    lazy loading init

--- AntraShare/src/app/app-routing.module.ts
+++ AntraShare/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NewsFeedComponent } from './features/news-feed-page/news-feed/news-feed.component';
 import { ProfilePageComponent } from './features/profile-page/profile-page.component';
 import { SettingPageComponent } from './features/setting-page/setting-page.component';
 import { LoginPageComponent } from './features/login-page/login-page.component';
 import { RegisterPageComponent } from './features/register-page/register-page.component';
 import { AdminPageComponent } from './features/admin-page/admin-page.component';
 import { AuthGuard } from './core/auth.guard';
+import { NewsFeedComponent } from './features/news-feed-page/news-feed/news-feed.component';
 
 const routes: Routes = [
   {
-    path: 'news', component: NewsFeedComponent
+    // path: 'news', component: NewsFeedComponent
+    path:'news',
+    component: NewsFeedComponent
   },
   {
     path: 'profile', component: ProfilePageComponent

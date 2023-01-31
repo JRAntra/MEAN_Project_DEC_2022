b82ce17de4513575b98fb939ff2b42c145b628b5
Tianshu Xiao <tx2@g.ucla.edu>
2023-01-31 09:57:24 -0800

    auth guard

--- AntraShare/src/app/core/auth.guard.ts
+++ AntraShare/src/app/core/auth.guard.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router){
+
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    
+    state: RouterStateSnapshot): any
+    // boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>
+    { 
     if (localStorage.getItem('role')== "admin"){
       return true;
-
     }else {
-      return false;
+      this.router.navigate(['/'])
     }
   }
+
   
 }
